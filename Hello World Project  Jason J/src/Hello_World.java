01581503afbe454446a3237cd52242627f8c8756
APCS2nd <APCS2nd@MHS-Dreyer-10>
2018-08-20 09:45:50 -0700

    First project to commit to Github for 2018

---
+++ Hello World Project  Jason J/src/Hello_World.java
@@ -0,0 +1,22 @@
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+/**
+ * Write a description of class Hello_world here.
+ * Print out a message saying hello
+ * @author Jason Ji 
+ * @version 8/17/2018
+ */
+public class Hello_world
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Jason"); //Display the string
+    }
+}
